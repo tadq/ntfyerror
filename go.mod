8f706c99054d6802bfaf21aebc0bba50f5891b7e
tadq <tadvizbaras@gmail.com>
2024-05-14 14:09:12 -0400

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tadq/ntfyerror
+
+go 1.22.0
+
+require (
+	github.com/AnthonyHewins/gotfy v0.0.9 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+)
---
+++ ntfyerror.go
@@ -0,0 +1,56 @@
+package ntfyerror
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"github.com/AnthonyHewins/gotfy"
+)
+
+type NtfyServer struct {
+	server  *url.URL
+	tp      *gotfy.Publisher
+	binName string
+}
+
+func New(ntfyURL string) *NtfyServer {
+	server, err := url.Parse(ntfyURL)
+	if err != nil {
+		panic("bad url:" + err.Error())
+	}
+
+	tp, err := gotfy.NewPublisher(server, http.DefaultClient)
+	if err != nil {
+		panic("bad config:" + err.Error())
+	}
+
+	var binName string
+	binaryName, err := os.Executable()
+	if err == nil {
+		binName = filepath.Base(binaryName)
+	}
+
+	return &NtfyServer{
+		server:  server,
+		tp:      tp,
+		binName: binName,
+	}
+}
+
+func (s NtfyServer) SendError(err error, tags ...string) {
+	_, errm := s.tp.SendMessage(context.Background(), &gotfy.Message{
+		Topic:    "alert",
+		Message:  err.Error(),
+		Title:    s.binName + " error",
+		Tags:     tags,
+		Priority: gotfy.High,
+	})
+
+	if errm != nil {
+		log.Println(errm)
+	}
+}
